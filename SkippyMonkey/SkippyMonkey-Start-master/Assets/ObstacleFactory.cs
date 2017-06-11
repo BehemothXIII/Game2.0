06057b2a8d8119c8b900cbb68bdfc347bafe8284
BehemothXIII <tranquangviet4695@gmail.com>
2017-06-11 20:30:10 +0700

    Instantiate Obstacle prefab vertical

---
+++ SkippyMonkey/SkippyMonkey-Start-master/Assets/ObstacleFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleFactory : MonoBehaviour {
+	public GameObject obstaclePrefab;
+	// Use this for initialization
+	void Start () {
+		for (int y = -195; y < 450; y += 200) {
+			Vector2 position = new Vector2 (Random.Range (-310, 310), y);
+			GameObject obstacle = Instantiate (obstaclePrefab, position, Quaternion.identity) as GameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
